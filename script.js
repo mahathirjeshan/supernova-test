ae0223945b150139d9f0deba963c0b9a5c33ba8d
mejshan <mahathir.jeshan@aivalabs.com>
2022-03-22 12:28:52 +0600

    design added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(true)
+$(".carousel--loop").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $(".carousel-item").length;
+
+    console.log(e.direction, idx);
+
+    if (idx >= totalItems - itemsPerSlide) {
+        var it = itemsPerSlide - (totalItems - idx);
+        console.log(itemsPerSlide, it, totalItems, idx);
+        for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+                $(".carousel-item").eq(i).appendTo(".carousel-inner");
+            } else {
+                $(".carousel-item").eq(0).appendTo(".carousel-inner");
+            }
+        }
+    }
+});
