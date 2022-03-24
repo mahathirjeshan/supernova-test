721fcd08c701a196866c7ec27f2a0bdca578bf8e
mejshan <mahathir.jeshan@aivalabs.com>
2022-03-24 19:48:40 +0600

    slider fixed

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ $(".carousel--loop").on("slide.bs.carousel", function (e) {
     var itemsPerSlide = 5;
     var totalItems = $(".carousel-item").length;
 
-    console.log(e.direction, idx);
-
-    if (idx >= totalItems - itemsPerSlide) {
+    if (idx > totalItems - itemsPerSlide) {
         var it = itemsPerSlide - (totalItems - idx);
-        console.log(itemsPerSlide, it, totalItems, idx);
         for (var i = 0; i < it; i++) {
-            // append slides to end
             if (e.direction == "left") {
                 $(".carousel-item").eq(i).appendTo(".carousel-inner");
             } else {
